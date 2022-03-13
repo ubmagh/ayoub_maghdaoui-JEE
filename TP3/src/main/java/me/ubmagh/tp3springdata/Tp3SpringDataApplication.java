b9ae20c2099d0b0a60ebf9718d0e37c50d6940b8
ayoub <ubmagh@gmail.com>
2022-03-13 23:23:25 +0100

    Refactoring

---
+++ TP3/src/main/java/me/ubmagh/tp3springdata/Tp3SpringDataApplication.java
@@ -0,0 +1,13 @@
+package me.ubmagh.tp3springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp3SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tp3SpringDataApplication.class, args);
+    }
+
+}
---
+++ TP3/src/test/java/me/ubmagh/tp3springdata/Tp3SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ubmagh.tp3springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
