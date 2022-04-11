b6620b98e6befbd4ba045464a9dc57e27e2eb20f
ayoub <ubmagh@gmail.com>
2022-04-11 09:28:39 +0000

    AP-gestion-etudiants: init project

---
+++ AP-Gestion-Etudiants/src/main/java/me/ubmagh/apgestionetudiants/ApGestionEtudiantsApplication.java
@@ -0,0 +1,13 @@
+package me.ubmagh.apgestionetudiants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApGestionEtudiantsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApGestionEtudiantsApplication.class, args);
+    }
+
+}
---
+++ AP-Gestion-Etudiants/src/test/java/me/ubmagh/apgestionetudiants/ApGestionEtudiantsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ubmagh.apgestionetudiants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApGestionEtudiantsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
